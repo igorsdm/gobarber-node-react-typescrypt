86f8ce6ae4093466910455f30433eeacff208826
Igor Moraes <igor@portalqualis.com.br>
2020-08-02 22:06:02 -0300

    backend base project / user, session and appointments

---
+++ backend/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '00bef104292169f961a49f2ba63512dc',
+    expiresIn: '1d',
+  },
+};
---
+++ backend/src/routes/session.routes.ts
@@ -0,0 +1,23 @@
+import { Router } from 'express';
+
+import CreateSessionService from '../services/CreateSessionService';
+
+const sessionRouter = Router();
+
+sessionRouter.post('/', async (request, response) => {
+  try {
+    const { email, password } = request.body;
+
+    const createSession = new CreateSessionService();
+
+    const { user, token } = await createSession.execute({ email, password });
+
+    delete user.password;
+
+    return response.json({ user, token });
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+export default sessionRouter;
