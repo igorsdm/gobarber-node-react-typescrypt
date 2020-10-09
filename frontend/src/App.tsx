76e0b43ac8f4f51cd47822f957150ced7aa25c60
Igor Moraes <igor_sdm@hotmail.com>
2020-10-09 16:01:10 -0300

    iniciando front end / projeto base / página singIn

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import GlobalStyles from './styles/global';
+import SignIn from './pages/SignIn';
+
+const App: React.FC = () => (
+  <>
+    <SignIn />
+    <GlobalStyles />
+  </>
+);
+
+export default App;
